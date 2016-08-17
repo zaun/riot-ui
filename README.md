14d567cc1b84bccc962dc26f21472600e9f6ccbe
Justin Zaun <justinzaun@Justins-iMac.local>
2016-08-16 17:42:40 -1000

    remove code

--- README.md
+++
@@ -1,2 +0,0 @@
-# riot-ui [![Build Status](https://travis-ci.org/zaun/riot-ui.svg?branch=master)](https://travis-ci.org/zaun/riot-ui)
-Riotjs UI tags based on the bootstrap css framework's look and feel
--- src/tags/rui-code/rui-code.js
+++
@@ -1,6 +0,0 @@
-self.on('update', function () {
-  self.dark = opts.hasOwnProperty('dark') ? opts.dark === '' || opts.dark === 'dark' || opts.dark === true : false;
-  self.lang = opts.hasOwnProperty('lang') ? opts.lang : 'HTML';
-
-
-});
